cdb37eb22abb465a562292f5d48f2332b6c2ad57
ErosKollerDev <eroskoller.dev@gmail.com>
2025-03-27 13:04:42 -0300

    first commit

---
+++ src/main/java/com/example/sbm_test_gmud/SbmTestGmudApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbm_test_gmud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmTestGmudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmTestGmudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbm_test_gmud/SbmTestGmudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbm_test_gmud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmTestGmudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
